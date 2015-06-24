34edef13bb1112bc9ddc15de2a90f81a928f6a12
David Anson <david@dlaa.me>
2015-06-23 18:31:32 -0700

    Implement apply, add tests.

--- promise-ring.js
+++ promise-ring.js
@@ -1,7 +1,7 @@
 "use strict";
 
-module.exports.call = function call(fn) {
-  var args = Array.prototype.slice.call(arguments, 1);
+exports.apply = function apply(fn, args) {
+  args = Array.isArray(args) ? args.slice() : [];
   return new Promise(function executor(resolve, reject) {
     args.push(function fnCallback(err) {
       if (err) {
@@ -12,4 +12,9 @@ module.exports.call = function call(fn) {
     });
     fn.apply(null, args);
   });
+}
+
+exports.call = function call(fn) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  return exports.apply(fn, args);
 };
--- test/promise-ring-test.js
+++ test/promise-ring-test.js
@@ -3,6 +3,10 @@
 var fs = require("fs");
 var pr = require("../promise-ring");
 
+function throwFn() {
+  throw new Error("throwFn");
+}
+
 module.exports = {
   callStatSuccess: function(test) {
     test.expect(3);
@@ -22,6 +26,61 @@ module.exports = {
         test.ok(err, "No Error instance.");
         test.ok(err instanceof Error, "Unexpected Error type.");
         test.equal(err.code, "ENOENT", "Incorrect Error code.");
+      })
+      .then(test.done, test.done);
+  },
+
+  callThrowFn: function(test) {
+    test.expect(3);
+    pr.call(throwFn)
+      .catch(function(err) {
+          test.ok(err, "No Error instance.");
+          test.ok(err instanceof Error, "Unexpected Error type.");
+          test.equal(err.message, "throwFn", "Incorrect Error message.");
+        })
+      .then(test.done, test.done);
+  },
+
+  applyStatSuccess: function(test) {
+    test.expect(3);
+    pr.apply(fs.stat, ["./package.json"])
+      .then(function(stats) {
+        test.ok(stats, "No Stats instance.");
+        test.ok(stats instanceof fs.Stats, "Unexpected Stats type.");
+        test.ok(stats.isFile(), "Incorrect Stats behavior.");
+      })
+      .then(test.done, test.done);
+  },
+
+  applyStatFailure: function(test) {
+    test.expect(3);
+    pr.apply(fs.stat, ["./missing.file"])
+      .then(null, function(err) {
+        test.ok(err, "No Error instance.");
+        test.ok(err instanceof Error, "Unexpected Error type.");
+        test.equal(err.code, "ENOENT", "Incorrect Error code.");
+      })
+      .then(test.done, test.done);
+  },
+
+  applyThrowFn: function(test) {
+    test.expect(3);
+    pr.apply(throwFn)
+      .catch(function(err) {
+          test.ok(err, "No Error instance.");
+          test.ok(err instanceof Error, "Unexpected Error type.");
+          test.equal(err.message, "throwFn", "Incorrect Error message.");
+        })
+      .then(test.done, test.done);
+  },
+
+  argsNotModified: function(test) {
+    test.expect(1);
+    var originalArgs = ["./package.json"];
+    var args = originalArgs.slice();
+    pr.apply(fs.stat, args)
+      .then(function(stats) {
+        test.deepEqual(args, originalArgs, "Arguments array modified.");
       })
       .then(test.done, test.done);
   }
